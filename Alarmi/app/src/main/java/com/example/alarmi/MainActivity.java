723079eb442c60f8a28341865027fba0b936c34c
simunovicmarko <59643387+simunovicmarko@users.noreply.github.com>
2020-11-09 14:58:22 +0100

    Dan1

--- Alarmi/app/src/main/java/com/example/alarmi/MainActivity.java
+++ Alarmi/app/src/main/java/com/example/alarmi/MainActivity.java
@@ -3,6 +3,14 @@ package com.example.alarmi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
+import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        SlidingUpPanelLayout layout  = findViewById(R.id.layout);
+
+        RelativeLayout oknoAlarm = (RelativeLayout)findViewById(R.id.oknoAlarmID);
+
+        oknoAlarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
+            }
+        });
+
+
+        layout.addPanelSlideListener(new SlidingUpPanelLayout.PanelSlideListener() {
+            @Override
+            public void onPanelSlide(View panel, float slideOffset) {
+            }
+
+            @Override
+            public void onPanelStateChanged(View panel, SlidingUpPanelLayout.PanelState previousState, SlidingUpPanelLayout.PanelState newState) {
+                if (newState == SlidingUpPanelLayout.PanelState.EXPANDED){
+                    Toast.makeText(MainActivity.this, "Expanded", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
     }
+
+
+
 }
